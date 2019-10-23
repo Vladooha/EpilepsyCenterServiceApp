278cf00212aa13ac887cef7e404e929fc1574737
vladooha <vladeeslavue@yandex.com>
2019-10-23 20:36:32 +0300

    Removed old frontend and backend folders

--- frontend/lib/service/login/loginService.dart
+++
@@ -1,9 +0,0 @@
-abstract class LoginService {
-  bool isLogged();
-  bool logIn(String login, String password);
-  bool register(String login, String password);
-  bool isValidLogin(String login);
-  bool isValidPassword(String password);
-  List<String> getLoginValidationErrors(String login);
-  List<String> getPasswordValidationErrors(String password);
-}
